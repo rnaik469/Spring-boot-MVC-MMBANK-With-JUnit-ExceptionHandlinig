0b78a44c15ce013c2632f6d75bd900b59cd7427f
rnaik469 <rnaik469@gmail.com>
2018-08-23 00:08:16 +0530

    Update InsufficientBalanceException.java

--- src/main/java/in/co/cg/spring/boot/mmbank/exception/InsufficientBalanceException.java
+++ src/main/java/in/co/cg/spring/boot/mmbank/exception/InsufficientBalanceException.java
@@ -3,7 +3,7 @@ package in.co.cg.spring.boot.mmbank.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE, reason="Insufficient Balance")  // 404
+@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE, reason="Insufficient Balance")  // 406
 public class InsufficientBalanceException extends RuntimeException {
 	public InsufficientBalanceException(String msg) {
 			super(msg);
